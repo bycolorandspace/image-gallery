bc0742ffadc38f45e1dcd392ca0cd3ab23d5a2ab
C E Chiejine <chike@Cs-MacBook-Pro.local>
2022-09-23 18:37:07 +0100

    Corrected revalidate error

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,10 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  name: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -18,11 +14,4 @@ export default async function handler(
   } catch (err) {
     return res.status(500).send("Error revalidating");
   }
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: "John Doe" });
 }
